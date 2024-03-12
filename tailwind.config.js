bc01179d9c96c50077a06a79f1925d3699b4ec4a
PalakShrivas04 <palakshrivas74@gmail.com>
2024-03-12 08:57:32 +0530

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      prime: "#212529",
+      secondary: "#343a40",
+      buttonPrimary: "#d90429",
+    },
   },
   plugins: [],
 };
